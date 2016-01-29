9c3e74cbd37514c7beb8568520673cbad1abc595
Daniel Konings <info@danielkonings.com>
2016-01-30 00:59:37 +0100

    Test dat controller

---
+++ GlobalGameJam16/Assets/Scripts/UI/RitualUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualUI : MonoBehaviour
+{
+    public Sprite aButtonSprite;
+    public Sprite bButtonSprite;
+    public Sprite xButtonSprite;
+    public Sprite yButtonSprite;
+    public Sprite leftButtonSprite;
+    public Sprite rightButtonSprite;
+    public Sprite upButtonSprite;
+    public Sprite downButtonSprite;
+
+    private GameController gameController_;
+
+	void Awake()
+    {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+
+        if (gameController == null)
+        {
+            Debug.LogError("Could not find a GameController object tagged with 'GameController'");
+            return;
+        }
+
+        gameController_ = gameController.GetComponent<GameController>();
+	}
+	
+	void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Joystick1Trigger") + " | " + Input.GetAxisRaw("Joystick2Trigger"));
+	}
+}
