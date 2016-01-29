46b44389e59be8a5e9cb76270b68359773e88039
Riko Ophorst <riko_ophorst@hotmail.com>
2016-01-30 00:59:54 +0100

    Laaaaaaaanes

---
+++ GlobalGameJam16/Assets/Scripts/Game/LaneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LaneManager : MonoBehaviour
+{
+    public List<Lane> lanes;
+
+    public void Awake()
+    {
+        lanes = new List<Lane>();
+    }
+}
